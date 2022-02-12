76b3ee72c80b3770e191325acfa7682e94ec3806
kleecollage <56179611+kleecollage@users.noreply.github.com>
2022-02-11 22:36:52 -0500

    Edicion Kevin
    modificacion de la pagina restaurants

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+//Modificacion Restaurante
+$(document).ready(function(){
+    $('ul.tabs li a:first').addClass('active');
+    $('.secciones article').hide();
+    $('.secciones article:first').show();
+
+    $('ul.tabs li a').click(function(){
+        $('ul.tabs li a').removeClass('active');
+        $(this).addClass('active');
+        $('.secciones article').hide();
+
+        var activeTab = $(this).attr('href');
+        $(activeTab).show();
+        return false;
+    });
+});
+
 (function ($) {
     "use strict";
     
@@ -65,4 +82,3 @@
 
     });
 })(jQuery);
-
