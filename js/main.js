8f99586d6957a497fb7bd9a15bf63eb0ec15b51b
AndyEmmanuel002 <56179613+AndyEmmanuel002@users.noreply.github.com>
2022-02-15 10:15:44 -0500

    Kevin
    Main.js actualización de main a restaurants pt2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+//Modificacion Restaurante
+$(document).ready(function(){
+    $('ul.tabs li a:first').addClass('active');
+    $('.secciones article').hide();
+    $('.secciones article:first').show();
+
+    $('ul.tabs li a').click(function(){
+        $('ul.tabs li a').removeClass('active');
+        $(this).addClass('active');
+        $('.secciones article').hide();
+
+        var activeTab = $(this).attr('href');
+        $(activeTab).show();
+        return false;
+    });
+});
+
 (function ($) {
     "use strict";
     
@@ -64,5 +81,5 @@
         });
 
     });
-})(jQuery);
 
+})(jQuery);
