859941e08e0939a744636c0dcc3ee8e5fd657adc
AndyEmmanuel002 <56179613+AndyEmmanuel002@users.noreply.github.com>
2022-03-23 06:59:49 -0500

    Creación de tablas con vue
    Se paso el código de Js a vue.js para  poder mostrar las tablas de una manera adecuada

--- admin2/src/components/views/Tables.vue
+++ admin2/src/components/views/Tables.vue
@@ -1,71 +1,75 @@
 <template>
   <section class="content">
-    
-<div class="container-fluid">
-
-                    <!-- DataTales Example -->
-                    <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Rosmarino Reservaciones</h6>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <th>Nombre</th>
-                                        <th>Apellido</th>
-                                        <th>Telefono</th>
-                                        <th>Email</th>
-                                        <th>Dia u Hora</th>        
-                                        <th>Solicitud especial</th>
-                                        <th>Numero de Personas</th>
-                                        <th>ID Restaurante</th>
-                                        <th>Acciones</th>
-                                        
-                                    </thead>
-                                    <tbody id="data">
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
+    <div class="container-fluid">
+      <!-- DataTales Example -->
+      <div class="card shadow mb-4">
+        <div class="card-header py-3">
+          <h6 class="m-0 font-weight-bold text-primary">
+            Rosmarino Reservaciones
+          </h6>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table
+              class="table table-bordered"
+              id="dataTable"
+              width="100%"
+              cellspacing="0"
+            >
+              <thead>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Telefono</th>
+                <th>Email</th>
+                <th>Dia u Hora</th>
+                <th>Solicitud especial</th>
+                <th>Numero de Personas</th>
+                <th>ID Restaurante</th>
+                <th>Acciones</th>
+              </thead>
+              <tbody id="data">
+                <tr v-for="record in records" :key="record.id">
+                    <td>{{record.nombre_usuario}}</td>
+                    <td>{{record.apellido_usuario}}</td>
+                    <td>{{record.telefono}}</td>
+                    <td>{{record.email}}</td>
+                    <td>{{record.dia_hora_booking}}</td>
+                    <td>{{record.solicitud_especial}}</td>
+                    <td>{{record.number_people}}</td>
+                    <td>{{record.restaurante_id}}</td>
+                    <td> 
+                    <a id="editBtn" href="#"> <i class="fas fa-edit"></i> </a>
+                    <a id="deleteBtn" href="#"> <i class="fas fa-user-times"></i> </a>
+                    <a id="readBtn" href="#"> <i class="fas fa-eye"></i> </a></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      records: []
+    }
+  },
+  mounted() {
+    let url = 'https://apicodeword12.herokuapp.com/booking/'
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.records = data.results
+      })
+      .catch((error) => console.log(error))
+  }
+}
 
-      let url = 'https://apicodeword12.herokuapp.com/booking/'
-      fetch(url)
-            .then(response => response.json())
-            .then(data => mostrarData(data))
-            .catch(error => console.log(error))
-
-      const mostrarData = (data) => {
-        console.log(data.results)
-        let body = ''
-        for (let i = 0; i < data.results.length; i++) {
-          body += `
-                <tr><td>${data.results[i].nombre_usuario}</td>
-                <td>${data.results[i].apellido_usuario}</td>
-                <td>${data.results[i].telefono}</td>
-                <td>${data.results[i].email}</td>
-                <td>${data.results[i].dia_hora_booking}</td>
-                <td>${data.results[i].solicitud_especial}</td>
-                <td>${data.results[i].number_people}</td>
-                <td>${data.results[i].restaurante_id}</td>
-                <td> 
-                <a id="editBtn" href="#"> <i class="fas fa-edit"></i> </a>
-                <a id="deleteBtn" href="#"> <i class="fas fa-user-times"></i> </a>
-                <a id="readBtn" href="#"> <i class="fas fa-eye"></i> </a></td> </tr>
-                `
-        }
-        document.getElementById('data').innerHTML = body
-      }
 </script>
 
 <style>
-
 </style>
--- admin2/src/components/views/Tasks.vue
+++ admin2/src/components/views/Tasks.vue
@@ -30,31 +30,7 @@
 
   </section>
 </template>
-<script>
-      let url = 'https://apicodeword12.herokuapp.com/usuarios/'
-      fetch(url)
-            .then(response => response.json())
-            .then(data => mostrarData(data))
-            .catch(error => console.log(error))
 
-      const mostrarData = (data) => {
-        console.log(data.results)
-        let body = ''
-        for (let i = 0; i < data.results.length; i++) {
-          body += `
-                <tr><td>${data.results[i].name}</td>
-                <td>${data.results[i].last_name}</td>
-                <td>${data.results[i].contactmail}</td>
-                <td>${data.results[i].cellphone}</td>
-                <td> 
-                <a id="editBtn" href="#"> <i class="fas fa-edit"></i> </a>
-                <a id="deleteBtn" href="#"> <i class="fas fa-user-times"></i> </a>
-                <a id="readBtn" href="#"> <i class="fas fa-eye"></i> </a></td> </tr>
-                `
-        }
-        document.getElementById('data').innerHTML = body
-      }
-</script>
 
 <style>
 
