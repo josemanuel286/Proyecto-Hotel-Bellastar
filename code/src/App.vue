b3c9c8d929e18f5abddd518b455539eba2c8f067
kleecollage <56179611+kleecollage@users.noreply.github.com>
2022-04-04 16:09:08 -0500

    Revert "Elimincación de carpeta code"
    This reverts commit d3ec4e861f387834037bbb5b5d1b502a2ff8f1af.

---
+++ code/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Login from "./components/login.vue";
+
+import Headers from "./components/headers.vue";
+import NavBar from "./components/NavBar.vue";
+
+</script>
+
+<template>
+  <header>
+    <Headers/>
+    
+    <div class="wrapper">
+      
+      <Login/>
+      
+    </div>
+    
+  </header>
+</template>
+
+<style>
+
+</style>
