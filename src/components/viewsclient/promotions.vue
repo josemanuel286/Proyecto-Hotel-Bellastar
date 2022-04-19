d20e99bc65e879f875f08264fb5ddf4fc0f96da8
AndyEmmanuel002 <56179613+AndyEmmanuel002@users.noreply.github.com>
2022-04-19 18:30:59 -0500

    promociones

--- src/components/viewsclient/promotions.vue
+++ src/components/viewsclient/promotions.vue
@@ -2,21 +2,27 @@
 
     <section id="promo">
         <clientheader></clientheader>
-        <div class="row"> 
-            
-            <div class="col-lg-12 col-md-8 col-sm-8  col-xs-12">
-                <div class="box-part text-center"> 
-                    <div class="title">
-                        <h2>titulo</h2>
-                        <div class="text">
-                            <h4>descripcion</h4>
-                            <iframe  width="500" height="609" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-                       </div>
-                       <h2>agotar existencias</h2>
+        <br><br><br>
+                    <div>
+                        <div class="box-part text-center">
+                            <h2>¡¡DESCUENTOS DEL 50%!!</h2>
+                            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D140730505189554%26id%3D115222261073712&show_text=true&width=500" width="500" height="456" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
+                    <br><br><br>
+                    <div>
+                        <div class="box-part text-center">
+                            <h2>¡¡DESCUENTOS DEL 50%!!</h2>
+                            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D136416442287627%26id%3D115222261073712&show_text=true&width=500" width="500" height="396" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                        </div>
+                    </div>
+                    <br><br><br>
+                    <div>
+                        <div class="box-part text-center">
+                            <h2>¡¡DESCUENTOS DEL 50%!!</h2>
+                            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D136411532288118%26id%3D115222261073712&show_text=true&width=500" width="500" height="376" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                        </div>
+                    </div>
     </section>
 </template>
 <script>
